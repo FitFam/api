c2ff94ad8f5559843698adfdbbf701edd96b8a54
Andy Kay <andykay.me@gmail.com>
2021-02-06 10:57:57 -0500

    put token in cookie

--- lib/fitfam_web/controllers/auth_controller.ex
+++ lib/fitfam_web/controllers/auth_controller.ex
@@ -1,6 +1,6 @@
 defmodule FitFamWeb.AuthController do
   use FitFamWeb, :controller
-  plug Ueberauth
+  plug(Ueberauth)
   alias Ueberauth.Strategy.Helpers
 
   def delete(conn, _params) do
@@ -17,7 +17,10 @@ defmodule FitFamWeb.AuthController do
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     with {:ok, %FitFam.Accounts.User{} = user} <- FitFam.Accounts.find_or_create(auth),
          {:ok, token, _claims} <- FitFam.Guardian.encode_and_sign(user) do
-      conn |> json(%{access_token: token})
+      conn =
+        conn
+        |> put_resp_cookie("auth_token", token)
+        |> json(%{access_token: token})
     end
   end
 end
